ab6f90b76fcfa6e7062f48dc28da891165f41752
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-07-09 14:00:02 +0530

    Swamy/08jul work (#2)
    * Fix header casing in README and add empty A.txt file
    
    * Add empty files for design patterns in multiple languages
    
    * Remove empty B.txt file from Behavioral Patterns directory
    
    * Add initial Visual Studio solution file for design patterns practice
    
    * Implement Singleton pattern with Logger class and update solution structure

---
+++ src/csharp/CreationalPatterns/SingletonDemos/SingletonLogger/Logger.cs
@@ -0,0 +1,29 @@
+﻿namespace SingletonLogger;
+
+internal class Logger
+{
+    private static readonly Lazy<Logger> _lazyLogger = new(() => new Logger());
+
+    /// <summary>
+    /// Instance
+    /// </summary>
+    public static Logger Instance
+    {
+        get
+        {
+            return _lazyLogger.Value;
+        }
+    }
+
+    protected Logger()
+    {
+    }
+
+    /// <summary>
+    /// SingletonOperation
+    /// </summary> 
+    public void Log(string message)
+    {
+        Console.WriteLine($"Message to log: {message}");
+    }
+}
---
+++ src/csharp/CreationalPatterns/SingletonDemos/SingletonLogger/Program.cs
@@ -0,0 +1,21 @@
+﻿using SingletonLogger;
+
+Console.Title = "Singleton";
+
+// call the property getter twice
+var instance1 = Logger.Instance;
+var instance2 = Logger.Instance;
+
+if (instance1 == instance2 && instance2 == Logger.Instance)
+{
+    Console.WriteLine("Instances are the same.");
+}
+
+instance1.Log($"Message from {nameof(instance1)}");
+
+instance1.Log($"Message from {nameof(instance2)}");
+
+Logger.Instance.Log($"Message from {nameof(Logger.Instance)}");
+
+Console.WriteLine("\n\nPress any key to exit...");
+Console.ReadLine();
